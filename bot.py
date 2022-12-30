1213b04b3dcd0079dd01750c3724522c3267a227
renamerbot1st <121661488+renamerbot1st@users.noreply.github.com>
2022-12-30 16:48:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ API_ID = int(os.environ.get("API_ID", ""))
 
 API_HASH = os.environ.get("API_HASH", "")
 
-STRING = os.environ.get("STRING", "")
+STRING = os.environ.get("STRING", "1BVtsOKkBuyu3NlXKI8vQUpHBZpepFMyjvQsHxpetHKvCfxDXn5ntUhXH8GL-sv5-scpu6rYk4dCG8rbyZ5U3CGD_7akNEjFUX6f4KTt9gndjSP6pQIMqc4pAQO_OtaXyR7bsp0DLSKdHVlNWeGgL57QvhZPPDOXpiScXNSHT0fNdAhz5V7dZzsULYkiyjGE2EHgees4DJHnRl7WmOSjgOf-D9FMVjXT4nHzjyEWimDbTlsT1O37mihovo3YnqoL9BRsvP_lE5vY-JvCZ5f6S9dAh_gqwfx7v5rrV8bc-u4sb_AuD__nZoYx-Oh8rOuoUzjV-o15JwPchZ1yHIH961KS2nNah7us=")
 
 
 bot = Client(
